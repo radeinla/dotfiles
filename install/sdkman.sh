43de967c2995169dae2adb3b4e4f69f09e8aba79
Deinla <rdeinla@c02vp2hnhtdd.agoda.local>
2018-03-07 15:09:55 +0700

    Do not reinstall sdkman if already installed.

--- install/sdkman.sh
+++ install/sdkman.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-curl -s "https://get.sdkman.io" | bash
+if [ -z "$(type sdk || echo '')" ]; then
+    curl -s "https://get.sdkman.io" | bash
+else
+    echo "sdkman installed";
+fi
 
 sdk install gradle
 sdk install sbt
 sdk install groovy
+sdk install scala
